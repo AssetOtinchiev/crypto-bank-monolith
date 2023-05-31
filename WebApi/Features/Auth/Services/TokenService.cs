891974d1c5d806e32b0c52e9ad79fa14ebdcd8f1
AssetKlips <asseto@klips.com>
2023-06-01 00:36:19 +0600

    feature/refresh-token [edit] GenerateRefreshToken non static

--- WebApi/Features/Auth/Services/TokenService.cs
+++ WebApi/Features/Auth/Services/TokenService.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using WebApi.Database;
+using WebApi.Features.Auth.Domain;
 using WebApi.Features.Users.Domain;
+using WebApi.Shared;
 
 namespace WebApi.Features.Auth.Services;
 
@@ -8,18 +10,38 @@ public class TokenService
 {
     private readonly AppDbContext _dbContext;
     private readonly TokenHelper _tokenHelper;
+    private readonly PasswordHelper _passwordHelper;
 
-    public TokenService(AppDbContext dbContext, TokenHelper tokenHelper)
+    public TokenService(AppDbContext dbContext, TokenHelper tokenHelper, PasswordHelper passwordHelper)
     {
         _dbContext = dbContext;
         _tokenHelper = tokenHelper;
+        _passwordHelper = passwordHelper;
     }
 
     public async Task<(string, string)> GenerateTokensAsync(User user, CancellationToken cancellationToken)
     {
         ArgumentNullException.ThrowIfNull(user);
         var accessToken = await _tokenHelper.GenerateAccessToken(user);
+         //var refreshToken = await _tokenHelper.GenerateRefreshToken();
         
+        // var salt = _passwordHelper.GetSecureSalt();
+        //
+        //  var refreshTokenHashed = _passwordHelper.HashUsingArgon2(refreshToken, salt);
+        //
+        //  if (user.RefreshTokens != null && user.RefreshTokens.Any())
+        //  {
+        //      await RemoveRefreshTokenAsync(user);
+        //  }
+        //
+        //  user.RefreshTokens?.Add(new RefreshToken
+        //  {
+        //      ExpiryDate = DateTime.Now.AddDays(14).ToUniversalTime(),
+        //      CreatedAt = DateTime.Now.ToUniversalTime(),
+        //      UserId = user.Id,
+        //      TokenHash = refreshTokenHashed,
+        //      TokenSalt = Convert.ToBase64String(salt)
+        //  });
         await _dbContext.SaveChangesAsync(cancellationToken);
 
         (string, string) token = (accessToken, "refreshToken");
