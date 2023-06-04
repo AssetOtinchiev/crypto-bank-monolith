ec5ee80b5c8e2ea9f046065c98f589456c7da583
AssetKlips <asseto@klips.com>
2023-06-04 13:29:58 +0600

    feature/refresh-token [edit] extra whitespace

--- WebApi/Features/Auth/Domain/RefreshToken.cs
+++ WebApi/Features/Auth/Domain/RefreshToken.cs
@@ -12,7 +12,7 @@ public class RefreshToken
     public DateTime ExpiryDate { get; set; }
     public bool IsRevoked { get; set; }
     public string DeviceName { get; set; }
-    public  Guid? ReplacedBy { get; set; }
-    
+    public Guid? ReplacedBy { get; set; }
+
     public virtual User User { get; set; }
 }
