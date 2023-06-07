e805a5fc377979ec300db13d11275141eed758d7
AssetKlips <asseto@klips.com>
2023-06-07 12:06:39 +0600

    feature/error-handle [add] HandleProblemDetails

--- WebApi/Errors/Extensions/ApplicationBuilderExtensions.cs
+++ WebApi/Errors/Extensions/ApplicationBuilderExtensions.cs
@@ -1,7 +1,6 @@
 using System.Diagnostics;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using FluentValidation;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Errors.Exceptions;
@@ -23,76 +22,35 @@ public static class ApplicationBuilderExtensions
                 {
                     case ValidationErrorsException validationErrorsException:
                     {
-                        var validationProblemDetails = new ProblemDetails
-                        {
-                            Title = "Validation failed",
-                            Type = "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/400",
-                            Detail = validationErrorsException.Message,
-                            Status = StatusCodes.Status400BadRequest,
-                        };
-
-                        validationProblemDetails.Extensions.Add("traceId",
-                            Activity.Current?.Id ?? context.TraceIdentifier);
+                        var validationProblemDetails = HandleProblemDetails("Validation failed",
+                            "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/400",
+                            400, validationErrorsException.Message, context);
 
                         validationProblemDetails.Extensions["errors"] = validationErrorsException.Errors
                             .Select(x => new ErrorDataWithCode(x.Field, x.Message, x.Code));
-
-                        context.Response.ContentType = "application/problem+json";
-                        context.Response.StatusCode = StatusCodes.Status400BadRequest;
 
                         await context.Response.WriteAsync(JsonSerializer.Serialize(validationProblemDetails));
                         break;
                     }
                     case LogicConflictException logicConflictException:
-                        var logicConflictProblemDetails = new ProblemDetails
-                        {
-                            Title = "Logic conflict",
-                            Type = "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/422",
-                            Detail = logicConflictException.Message,
-                            Status = StatusCodes.Status422UnprocessableEntity,
-                        };
-
-                        logicConflictProblemDetails.Extensions.Add("traceId",
-                            Activity.Current?.Id ?? context.TraceIdentifier);
-
+                        var logicConflictProblemDetails = HandleProblemDetails("Logic conflict",
+                            "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/422",
+                            422, logicConflictException.Message, context);
                         logicConflictProblemDetails.Extensions["code"] = logicConflictException.Code;
-
-                        context.Response.ContentType = "application/problem+json";
-                        context.Response.StatusCode = StatusCodes.Status422UnprocessableEntity;
 
                         await context.Response.WriteAsync(JsonSerializer.Serialize(logicConflictProblemDetails));
                         break;
                     case OperationCanceledException:
-                        var operationCanceledProblemDetails = new ProblemDetails
-                        {
-                            Title = "Timeout",
-                            Type = "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/504",
-                            Detail = "Request timed out",
-                            Status = StatusCodes.Status504GatewayTimeout,
-                        };
-
-                        operationCanceledProblemDetails.Extensions.Add("traceId",
-                            Activity.Current?.Id ?? context.TraceIdentifier);
-
-                        context.Response.ContentType = "application/problem+json";
-                        context.Response.StatusCode = StatusCodes.Status504GatewayTimeout;
+                        var operationCanceledProblemDetails = HandleProblemDetails("Timeout",
+                            "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/504",
+                            504, "Request timed out", context);
 
                         await context.Response.WriteAsync(JsonSerializer.Serialize(operationCanceledProblemDetails));
                         break;
                     default:
-                        var internalErrorProblemDetails = new ProblemDetails
-                        {
-                            Title = "Internal server error",
-                            Type = "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/500",
-                            Detail = "Interval server error has occured",
-                            Status = StatusCodes.Status500InternalServerError,
-                        };
-
-                        internalErrorProblemDetails.Extensions.Add("traceId",
-                            Activity.Current?.Id ?? context.TraceIdentifier);
-
-                        context.Response.ContentType = "application/problem+json";
-                        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                        var internalErrorProblemDetails = HandleProblemDetails("Internal server error",
+                            "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/500",
+                            500, "Interval server error has occured", context);
 
                         await context.Response.WriteAsync(JsonSerializer.Serialize(internalErrorProblemDetails));
                         break;
@@ -101,6 +59,26 @@ public static class ApplicationBuilderExtensions
         });
 
         return app;
+    }
+
+    private static ProblemDetails HandleProblemDetails(
+        string title, string type, int status, string message,
+        HttpContext context)
+    {
+        var validationProblemDetails = new ProblemDetails
+        {
+            Title = title,
+            Type = type,
+            Detail = message,
+            Status = status,
+        };
+
+        validationProblemDetails.Extensions.Add("traceId",
+            Activity.Current?.Id ?? context.TraceIdentifier);
+
+        context.Response.ContentType = "application/problem+json";
+        context.Response.StatusCode = status;
+        return validationProblemDetails;
     }
 }
 
