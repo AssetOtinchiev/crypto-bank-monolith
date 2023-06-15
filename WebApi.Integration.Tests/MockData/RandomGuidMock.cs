b9be95b3d73b8609b6e7ec715452da5c0496f56c
AssetKlips <asseto@klips.com>
2023-06-16 01:09:47 +0600

    feature/tests [edit] RandomGuidMock

--- WebApi.Integration.Tests/MockData/RandomGuidMock.cs
+++ WebApi.Integration.Tests/MockData/RandomGuidMock.cs
@@ -2,13 +2,11 @@ namespace WebApi.Integration.Tests.Features.Users.MockData;
 
 public static class RandomGuidMock
 {
-    public static IEnumerable<object[]> Guids
-    {
-        get
+    public static IEnumerable<object[]> Guids =>
+        new List<object[]>
         {
-            yield return new object[] {Guid.Parse("b3548ecf-8f31-4b4a-a120-1536fea7b3a7")};
-            yield return new object[] {Guid.Parse("7dd27c42-87fb-4d9c-a06f-38cd0fc7de0a")};
-            yield return new object[] {Guid.Empty};
-        }
-    }
+            new object[] {Guid.Parse("b3548ecf-8f31-4b4a-a120-1536fea7b3a7")},
+            new object[] {Guid.Parse("7dd27c42-87fb-4d9c-a06f-38cd0fc7de0a")},
+            new object[] {Guid.Empty}
+        };
 }
