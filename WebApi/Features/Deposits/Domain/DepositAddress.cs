c67567d800f85937de6c8b45354d6c7e9d5a5cc2
AssetKlips <asseto@klips.com>
2023-07-24 10:53:08 +0600

    feature/bitcoin-generate-address [edit] removed comments

--- WebApi/Features/Deposits/Domain/DepositAddress.cs
+++ WebApi/Features/Deposits/Domain/DepositAddress.cs
@@ -4,12 +4,12 @@ namespace WebApi.Features.Deposits.Domain;
 
 public class DepositAddress
 {
-    public int Id { get; set; } // Автогенерируемый идентификатор
-    public string CurrencyCode { get; set; } // Код валюты. Сейчас всегда будет BTC
-    public Guid UserId { get; set; }  // Идентификатор пользователя, которому принадлежит адрес
+    public int Id { get; set; }
+    public string CurrencyCode { get; set; }
+    public Guid UserId { get; set; }
     public User User { get; set; }
-    public int XpubId { get; set; }  // Идентификатор xpub, использованного для генерации адреса
+    public int XpubId { get; set; }
     public Xpub Xpub { get; set; } 
-    public int DerivationIndex { get; set; }  // Индекс, использованный для генерации адреса
-    public string CryptoAddress { get; set; }  // Непосредственно адрес
+    public int DerivationIndex { get; set; }
+    public string CryptoAddress { get; set; }
 }
--- WebApi/Features/Deposits/Domain/Xpub.cs
+++ WebApi/Features/Deposits/Domain/Xpub.cs
@@ -2,7 +2,7 @@ namespace WebApi.Features.Deposits.Domain;
 
 public class Xpub
 {
-    public int Id { get; set; } // Автогенерируемый идентификатор
-    public string CurrencyCode { get; set; } // Код валюты. Сейчас всегда будет BTC
-    public string Value { get; set; }  // Значение ключа. Например "xpub67xpozcx8pe95XVuZLHXZeG6XWXHpGq6Qv5cmNfi7cS5mtjJ2tgypeQbBs2UAR6KECeeMVKZBPLrtJunSDMstweyLXhRgPxdp14sk9tJPW9"
+    public int Id { get; set; }
+    public string CurrencyCode { get; set; }
+    public string Value { get; set; }
 }
