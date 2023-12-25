c5644ce8a426e65a3afda24889268150efa4a70c
AssetKlips <asseto@klips.com>
2023-12-25 18:05:04 +0600

    feature/terraform [add] Azure virtual machine

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -15,7 +15,24 @@ provider "azurerm" {
   features {}
 }
 
+locals {
+  resource_group="app-grp"
+  location="North Europe"
+}
+
 resource "azurerm_resource_group" "app_grp" {
-  name = "app-grp"
-  location = "North Europe"
+  name = local.resource_group
+  location = local.location
+}
+
+resource "azurerm_virtual_network" "app_network" {
+  name                = "app-network"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.app_grp.name
+  address_space       = ["10.0.0.0/16"]
+
+  subnet {
+    name           = "SubnetA"
+    address_prefix = "10.0.1.0/24"
+  }  
 }
